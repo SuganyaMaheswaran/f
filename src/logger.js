b92e160ca9dc8d3778887fad84f87726c09ccacf
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-11 16:30:17 -0500

    Add pino logger

---
+++ src/logger.js
@@ -0,0 +1,15 @@
+//create and configure Pino Logger instance ti kig various types of information:
+
+// Use `info` as our standard log level if not specified 
+const options = {level: process.env.LOG_LEVEL || 'info'};
+
+// If we're doing `debug` logging, make the logs easier to read
+if(options.level==='debug'){
+    options.transport={
+        target:'pino-pretty',
+        options:{
+            colorize: true,
+        }
+
+    }
+}
