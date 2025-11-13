d9e217b760c58e48ab8e9e801cd27238de378780
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-13 18:01:16 -0500

    enable Jest environment to avoid lint errors in tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,18 @@
 
 import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from '@eslint/js';
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules:{"no-unused-vars":"warn"}, languageOptions: { globals: globals.node } },
-  
+export default defineConfig([  
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    languageOptions:{
+      globals:{
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
 ]);
