386b105d8a129bc7353d2be1a63b934941a47703
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-13 12:31:57 -0500

    added main entry-point for the v1 version fo the fragments api

---
+++ src/routes/api/index.js
@@ -0,0 +1,15 @@
+// This will define the fragments api endpoints
+
+/**
+ * The main entry-point for the v1 version of the fragments API. 
+ */
+
+const express = require('express');
+
+// Create a router on which to mount our API endpoints
+const router = express.Router();
+
+// Define our first route, which will be: GET /v1/fragments
+router.get('/fragments',require('./get'));
+
+module.exports = router;
