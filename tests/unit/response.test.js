0d4397fd0f41513b751ca2945b5b68b9cb975e1f
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-13 18:44:03 -0500

    added unit test for response.js

---
+++ tests/unit/response.test.js
@@ -0,0 +1,43 @@
+// Unit test for response.js 
+
+const {createErrorResponse, createSuccessResponse} = require('../../src/response')
+
+// Test for calling createErrorResponse
+
+describe('API Responses',()=>{
+  test('createErrorResponse',()=>{
+    const errorResponse = createErrorResponse(404,'not found');
+    // Expect the result to look like the following
+    expect(errorResponse).toEqual({
+        status:'error',
+        error:{
+            code:404,
+            message:'not found',
+        },
+    });
+  }); 
+
+// Test for calling createSuccessResponse() with no argument
+test('createSuccessResponse()', ()=>{
+    // No arg passed
+    const successResponse = createSuccessResponse();
+    //Expect the result to look like the following
+    expect(successResponse).toEqual({
+        status:'ok',
+    });
+});
+
+
+// Test for calling createSuccessResponse() with an argument
+test('createSuccessResponse(data)', ()=>{
+    // Data argument included
+    const data = {a: 1, b:2};
+    const successResponse = createSuccessResponse(data);
+    // Expect teh result to look like the following
+    expect(successResponse).toEqual({
+        status:'ok',
+        a:1,
+        b:2,
+    })
+})
+});
