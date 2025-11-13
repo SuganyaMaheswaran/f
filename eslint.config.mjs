4f24b66bb01824e6c5c55f16031afe577260ba44
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-13 13:24:26 -0500

    added comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
+// This file is created by setup process 'npm init @eslint/config@latest'
+// It is configured for node.js using CommonJS modules,
+//  and uses good defaults for linting JavaScript projects. 
+// You can read more about how to configure eslint if you're interested, 
+// or want to override defaults.
+
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules:{"no-unused-vars":"warn"}, languageOptions: { globals: globals.node } },
+  
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
