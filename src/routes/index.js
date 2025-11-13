23437b7954a3e82897ac9c5ec79178da8656d418
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-13 12:28:39 -0500

    set up API v1 entry point with health check

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+// This will entry point for routes 
+
+const express = require('express');
+
+// Version and author from package.json
+const {version, author} = require('../../package.json');
+
+// Create a router that we can use to mount our API
+const router = express.Router(); 
+
+/** 
+ * Expose all our API routes on /v1/* to include an API version.
+ */
+
+router.use(`/v1`, require('./api'));
+
+/**
+ * Define a simple health check route. If the server is running 
+ * we'll respond with a 200 OK. If not, the server isn't healthy.
+ */
+
+
+router.get('/', (req,res)=>{
+    // Client's shouldn't cache this response (always a request it fresh);
+    res.setHeader('Cache-Control', 'no-cache');
+    // Send a 200 'OK' respones
+    res.status(200).json({
+        status:'ok',
+        author,
+        githubUrl:'https://github.com/SuganyaMaheswaran/fragments',
+        version,
+    });
+});
+
+module.exports = router;
