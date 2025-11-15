cae210066a274df06b1f6909d6491f661d40ccfb
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-15 13:01:21 -0500

    fix(tests): make unauthenticated request test async

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -3,12 +3,12 @@ const app = require('../../src/app');
 
 describe('GET /v1/fragments', ()=>{
     // If the request is missing the Authorization header, it should be forbidden
-    test('unauthenticated requests are denied', ()=>{
-        request(app).get('/v1/fragments').expect(401);
+    test('unauthenticated requests are denied', async ()=>{
+       await request(app).get('/v1/fragments').expect(401);
     });
     // If the wrong username/password pair are used (no such user), it should be forbidden
-    test('incorrect credentials are denied', ()=>{
-        request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401);
+    test('incorrect credentials are denied', async ()=>{
+       await request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401);
     })
     // Using a valid username/password pair should give a success result with a framgents array
     test('authenticated users get a fragments array', async ()=>{
