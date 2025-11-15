9b1b9506d2b747f45ed9189986528fcf70143de5
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-15 08:54:47 -0500

    Add 404 middleware to handle unfounc resources for request

--- src/app.js
+++ src/app.js
@@ -40,23 +40,32 @@ app.use(passport.initialize());
 // Define our HTTP routes(s)
 app.use('/', require('./routes'))
 
+
+
+// Add 404 middleware to handle any requests for resources that can't be found can't be found
+app.use((req, res) => {
+  // Pass along an error object to the error-handling middleware
+  res.status(404).json({
+    status: 'error',
+    error: {
+      message: 'not found',
+      code: 404,
+    },
+  });
+});
+
+
 // Error-handling middleware to deal with anything else 
 // eslint-disable-next-line no-unused-vars
 
-app.use((err,req, res, next)=>{
+app.use((err, req, res, next)=>{
     const status = err.status || 500
     const  message = err.message || 'unable to process request';
 
     if(status > 499){
         logger.error({err}, `Error processing request`)
     }
-    res.status(status).json({
-        status: 'error',
-        error:{
-            message,
-            code:status,
-        },
-    });
+    res.status(status).json(createErrorResponse(status, message));
 });
 
 
