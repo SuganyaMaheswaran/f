56e216da159c6106c61cd15f198c4548f1fcd0f2
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-11-16 17:54:33 -0500

    implement in-memory db

---
+++ src/model/data/memory/memory-dbs.js
@@ -0,0 +1,84 @@
+// This file is for in memory db implementation
+
+const { memo } = require("react");
+
+const validateKey = (key) => typeof key === 'string';
+
+class MemoryDB {
+  constructor() {
+    /** @type {Record<string, any>} */
+    this.db = {};
+  }
+
+  /**
+   * Gets a value for the give primaryKey and secondaryKey
+   * @param {string} primaryKey
+   * @param {string} secondaryKey
+   * @returns {Promise<any>}
+   */
+  get(primaryKey, secondaryKey) {
+    if (!(validateKey(primaryKey) && validateKey(secondaryKey))) {
+      throw new Error(
+        `primaryKey and secondaryKey strings are required, got primaryKey=${primaryKey}, secondaryKey=${secondaryKey}`
+      );
+    }
+    const db = this.db;
+    const value = db[primaryKey] && db[primaryKey][secondaryKey];
+    return Promise.resolve(value);
+  }
+
+  /**
+   * Puts a value into the given primaryKey and secondaryKey
+   * @param {string} primaryKey
+   * @param {string} secondaryKey
+   * @returns {Promise<void>}
+   */
+
+  put(primaryKey, secondaryKey, value) {
+    if (!(validateKey(primaryKey) && validateKey(secondaryKey))) {
+      throw new Error(
+        `primaryKey and secondaryKey strings are required, get primaryKey=${primaryKey}, secondaryKey=${secondaryKey}`
+      );
+    }
+    const db = this.db;
+    //Make sure the `primaryKey` exists, or create
+    db[primaryKey] = db[primaryKey] || {};
+    // Add the `value` to the `secondaryKey`
+    db[primaryKey][secondaryKey] = value;
+    return Promise.resolve();
+  }
+  /**
+   * Queries the list of value (i.e., secondaryKeys) for the given primaryKey.
+   * Always returns an Array, even if no items are found.
+   * @param {string} primaryKey
+   * @returns {Promise<any[]>}
+   */
+  query(primaryKey) {
+    if (!validateKey(primaryKey)) {
+      throw new Error(`primaryKey string is required, got pimaryKey=${primaryKey}`);
+    }
+    const db = this.db;
+    const values = db[primaryKey] ? Object.values(db[primaryKey]) : [];
+    return Promise.resolve(values);
+  }
+
+  /**
+   * Deletes the value with the given primaryKey and secondarykey
+   * @param {string} primaryKey
+   * @param {string} secondaryKey
+   * @returns {Promise<void>}
+   * 
+   */
+  async del(primaryKey, secondaryKey){
+    if(!(validateKey(primaryKey) && validateKey(secondaryKey))){
+        throw new Error(
+            `primaryKey and secondaryKey strings are required, got primaryKey=${primaryKey}, secondaryKey=${secondaryKey}`
+        );    
+    }
+    const db = this.db;
+    delete db[primaryKey][secondaryKey];
+    return Promise.resolve();
+  }
+
+}
+module.exports = MemoryDB
